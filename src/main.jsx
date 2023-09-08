6769f15ddebe66d4fdbf6d25c24ea49660869919
Vishal27-com <vishalgupta962148@gmail.com>
2023-09-08 13:22:06 +0530

    context provided

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { CryptoContextProvider } from './Context/CryptoContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CryptoContextProvider>
     <App />
+    </CryptoContextProvider>
   </React.StrictMode>,
 )
